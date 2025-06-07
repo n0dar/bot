2dde3e77d75d0c1cd92daa3d4ae397b511674ed5
n0dar <ru@ru.ru>
2025-06-08 02:55:45 +0300

    Урок 10. ДЗ

--- Program.cs
+++ Program.cs
@@ -1,32 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace bot
 {
     class Program
     {
-        static void DoStart(out string userName)
+        private static bool isStarted = false;
+        private static string userName="";
+        private static string command;
+        private static List<string> taskList = [];
+        static string GetStringDependsOnUserName(string str)
+        {
+            return 
+                (userName != "" ? userName + ", " : "") + 
+                (userName != "" ? char.ToLower(str[0]) : char.ToUpper(str[0])) + str[1..];
+        }
+        static void DoStart()
         {
             do
             {
                 Console.WriteLine("Введите ваше имя...\r\n");
                 userName = Console.ReadLine();
+                
             }
             while (userName.Trim() == "");
+            if (userName == "/exit") command = "/exit";
+            else Console.Clear();
         }
-        static void DoHelp(bool fStarted, string userName="")
+        static void AddTask()
+        {
+            Console.WriteLine(GetStringDependsOnUserName("Введите описание задачи...\r\n"));
+            taskList.Add(Console.ReadLine());
+            Console.WriteLine("Задача добавлена.\r\n");
+        }
+        static void ShowTasks(string msg)
+        {
+            if (taskList.Count > 0)
+            {
+                uint i = 0;
+                Console.WriteLine(GetStringDependsOnUserName(msg));
+                foreach (string task in taskList)
+                {
+                    Console.WriteLine($"{++i} {task}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Список задач пуст.");
+            }
+            Console.WriteLine("\r\n");
+        }
+        static void RemoveTask()
+        {
+            if (taskList.Count > 0)
+            {
+                ShowTasks("Укажите номер задачи, которую хотите удалить:");
+                if (int.TryParse(Console.ReadLine(), out int taskNumber))
+                {
+                    if (taskNumber != 0 && taskNumber <= taskList.Count)
+                    {
+                        taskList.RemoveAt(--taskNumber);
+                        Console.WriteLine("Задача удалена.\r\n");
+                    }
+                    else Console.WriteLine("Задача с таким номером не существует.\r\n");
+                }
+                else Console.WriteLine("Некорректный номер задачи.\r\n");
+            }
+            else Console.WriteLine("Список задач пуст.\r\n");
+        }
+        static void DoHelp()
         {
             Console.WriteLine
             (
-                (userName!="" ? userName + ", " : "") +
-                (userName!="" ? "д" : "Д") + "ля взаимодействия со мной вам доступен следующий список команд:\r\n\r\n" +
-                "/" + (fStarted ? "echo  — отображу введенную вами строку текста, указанную после команды через пробел" : "start — начните работу с этой команды") + ";\r\n" +
-                "/help  — покажу справочную информацию;\r\n" +
-                "/info  — покажу свои версию и дату создания;\r\n" +
-                "/exit  — завершу работу.\r\n" +
+                GetStringDependsOnUserName("Для взаимодействия со мной вам доступен следующий список команд:\r\n\r\n") +
+                (isStarted ? "" : "/start      — начните работу с этой команды" + ";\r\n")  +
+                (isStarted ?      "/addtask    — добавлю задачу в список" + ";\r\n" : "") +
+                (isStarted ?      "/showtasks  — покажу список задач" + ";\r\n" : "") +
+                (isStarted ?      "/removetask — удалю задачу из списка" + ";\r\n" : "") +
+                (isStarted ?      "/echo       — отображу введенную вами строку текста, указанную после команды через пробел" + ";\r\n" : "")  +
+                                  "/help       — покажу справочную информацию;\r\n" +
+                                  "/info       — покажу свои версию и дату создания;\r\n" +
+                                  "/exit       — завершу работу.\r\n" +
                 "Завершайте ввод нажатием на Enter\r\n"
             );
         }
-        static void DoInfo(string userName)
+        static void DoInfo()
         {
             Console.WriteLine
             (
@@ -40,12 +98,8 @@ namespace bot
         }
         static void Main()
         {
-            bool fStarted = false;
-            string command;
-            string userName = "";
-            
             Console.WriteLine("Привет! Я — бот. \r\n\r\n");
-            DoHelp(fStarted);
+            DoHelp();
 
             do
             {
@@ -54,31 +108,31 @@ namespace bot
                 switch (command)
                 {
                     case "/start":
-                        if (!fStarted)
+                        if (!isStarted)
                         {
-                            DoStart(out userName);
-                            fStarted = true;
+                            DoStart();
+                            isStarted = true;
                         }
-                        else
-                        { 
-                            DoHelp(fStarted, userName);
-                        }
+                        DoHelp();
+                        break;
+                    case "/addtask":
+                        if (isStarted) AddTask();
+                        break;
+                    case "/showtasks":
+                        if (isStarted) ShowTasks("Задачи в списке:");
+                        break;
+                    case "/removetask":
+                        if (isStarted) RemoveTask();
                         break;
                     case "/help":
-                        DoHelp(fStarted, userName);
+                        DoHelp();
                         break;
                     case "/info":
-                        DoInfo(userName);
+                        DoInfo();
                         break;
                     default:
-                        if (command.StartsWith("/echo ") && command.Length>6)
-                        {
-                            DoEcho(command);
-                        }
-                        else
-                        {
-                            DoHelp(fStarted, userName);
-                        }
+                        if (command.StartsWith("/echo ") && command.Length>6) DoEcho(command);
+                        else DoHelp();
                         break;
                 }
             }
