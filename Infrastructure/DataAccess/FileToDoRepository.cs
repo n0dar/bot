2189686a04a1fecd62768fd7f9cc75aadf468e6f
n0dar <ru@ru.ru>
2025-09-22 02:23:05 +0300

    Урок 25. ДЗ (п. 4)

--- Infrastructure/DataAccess/FileToDoRepository.cs
+++ Infrastructure/DataAccess/FileToDoRepository.cs
@@ -16,12 +16,55 @@ namespace bot.Infrastructure.DataAccess
     internal class FileToDoRepository: IToDoRepository
     {
         private readonly string _path;
+        private readonly string _indexFilePath;
         public FileToDoRepository(string Path)
         {
             _path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path) ;
             Directory.CreateDirectory(_path);
+            _indexFilePath = System.IO.Path.Combine(_path, "index.json");
         }
-        private async Task<IReadOnlyList<ToDoItem>> Get(Guid userId, Func<ToDoItem, bool> predicate, CancellationToken ct)
+        private static async Task<string> ObjestToJsonStringAsync(object obj, CancellationToken ct)
+        {
+            await using MemoryStream jsonStream = new();
+            await JsonSerializer.SerializeAsync(jsonStream, obj, cancellationToken: ct);
+            jsonStream.Position = 0;
+            using StreamReader jsonStreamReader = new(jsonStream, Encoding.UTF8);
+            return await jsonStreamReader.ReadToEndAsync(ct);
+        }
+        private async Task BuildIndexAsync(CancellationToken ct)
+        {
+            IEnumerable<string> files = Directory
+                .EnumerateFiles(_path, $"*.json", SearchOption.AllDirectories)
+                .Where(file => System.IO.Path.GetFileName(file) != "index.json");
+
+            if (files.Any())
+            {
+                Dictionary<Guid, Guid> index = [];
+                foreach (string file in files)
+                {
+                    using FileStream jsonFileStream = File.OpenRead(file);
+                    ToDoItem item = await JsonSerializer.DeserializeAsync<ToDoItem>(jsonFileStream, cancellationToken: ct);
+                    index.Add(item.Id, item.User.UserId);
+                }
+                await File.WriteAllTextAsync(_indexFilePath, await ObjestToJsonStringAsync(index, ct), ct);
+            }
+        }
+        private async Task<Dictionary<Guid, Guid>> ReadIndexAsync(CancellationToken ct)
+        {
+            if (File.Exists(_indexFilePath))
+            {
+                using (FileStream jsonFileStream = File.OpenRead(_indexFilePath))
+                {
+                    return await JsonSerializer.DeserializeAsync<Dictionary<Guid, Guid>>(jsonFileStream, cancellationToken: ct);
+                }
+            }
+            return null;
+        }
+        private async Task UpdateIndexAsync(Dictionary<Guid, Guid> index, CancellationToken ct)
+        {
+            await File.WriteAllTextAsync(_indexFilePath, await ObjestToJsonStringAsync(index, ct), ct);
+        }
+        private async Task<IReadOnlyList<ToDoItem>> GetItemsAsync(Guid userId, Func<ToDoItem, bool> predicate, CancellationToken ct)
         {
             List<ToDoItem> res = [];
             string userPath = Path.Combine(_path, $"{userId}");
@@ -38,13 +81,15 @@ namespace bot.Infrastructure.DataAccess
         }
         async Task IToDoRepository.AddAsync(ToDoItem item, CancellationToken ct)
         {
-            await using MemoryStream jsonStream = new();
-            await JsonSerializer.SerializeAsync(jsonStream, item, cancellationToken: ct);
-            jsonStream.Position = 0;
-            using StreamReader jsonStreamReader = new(jsonStream, Encoding.UTF8);
-            string jsonString = await jsonStreamReader.ReadToEndAsync(ct);
             Directory.CreateDirectory(Path.Combine(_path, $"{item.User.UserId}"));
-            await File.WriteAllTextAsync(Path.Combine(_path, $"{item.User.UserId}", $"{item.Id}.json"), jsonString, ct);
+            await File.WriteAllTextAsync(Path.Combine(_path, $"{item.User.UserId}", $"{item.Id}.json"), await ObjestToJsonStringAsync(item, ct), ct);
+            if (File.Exists(_indexFilePath))
+            {
+                Dictionary<Guid, Guid> index = await ReadIndexAsync(ct);
+                index.Add(item.Id, item.User.UserId);
+                await UpdateIndexAsync(index, ct);
+            }
+            else await BuildIndexAsync(ct);
         }
         async Task<int> IToDoRepository.CountActiveAsync(Guid userId, CancellationToken ct)
         {
@@ -63,16 +108,15 @@ namespace bot.Infrastructure.DataAccess
         }
         async Task IToDoRepository.DeleteAsync(Guid id, CancellationToken ct)
         {
-            ToDoItem item = await ((IToDoRepository)this).GetAsync(id, ct);
-            if (item != null)
-            { 
-                await Task.Run(() =>
-                {
-                    ct.ThrowIfCancellationRequested();
-                    string filePath = Path.Combine(_path, $"{item.User.UserId}", $"{id}.json");
-                    if (File.Exists(filePath)) File.Delete(filePath);
-                    else throw new TaskDoesNotExistException("Задача с таким GUID не существует");
-                }, ct);
+            Dictionary<Guid, Guid> index = await ReadIndexAsync(ct);
+
+            if (index.TryGetValue(id, out Guid userId))
+            {
+                index.Remove(id);
+                await UpdateIndexAsync(index, ct);
+                string filePath = Path.Combine(_path, $"{userId}", $"{id}.json");
+                if (File.Exists(filePath)) File.Delete(filePath);
+                else throw new TaskDoesNotExistException("Задача с таким GUID не существует");
             }
         }
         async Task<bool> IToDoRepository.ExistsByNameAsync(Guid userId, string name, CancellationToken ct)
@@ -91,29 +135,31 @@ namespace bot.Infrastructure.DataAccess
         }
         async Task<IReadOnlyList<ToDoItem>> IToDoRepository.FindAsync(Guid userId, Func<ToDoItem, bool> predicate, CancellationToken ct)
         {
-            return await Get(userId, predicate, ct);
+            return await GetItemsAsync(userId, predicate, ct);
         }
         async Task<IReadOnlyList<ToDoItem>> IToDoRepository.GetActiveByUserIdAsync(Guid userId, CancellationToken ct)
         {
-            return await Get(userId, item => item.State == ToDoItemState.Active, ct);
+            return await GetItemsAsync(userId, item => item.State == ToDoItemState.Active, ct);
         }
         async Task<IReadOnlyList<ToDoItem>> IToDoRepository.GetAllByUserIdAsync(Guid userId, CancellationToken ct)
         {
-            return await Get(userId, item => true, ct);
+            return await GetItemsAsync(userId, item => true, ct);
         }
         async Task<ToDoItem> IToDoRepository.GetAsync(Guid id, CancellationToken ct)
         {
-            IEnumerable<string> files = Directory.EnumerateFiles(_path, $"{id}.json", SearchOption.AllDirectories);
-            if (files.Any()) 
+            Dictionary<Guid, Guid> index = await ReadIndexAsync(ct);
+
+            if (index.TryGetValue(id, out Guid userId))
             {
-                using FileStream jsonFileStream = File.OpenRead(files.First());
+                using FileStream jsonFileStream = File.OpenRead(Path.Combine(_path, $"{userId}", $"{id}.json"));
                 return await JsonSerializer.DeserializeAsync<ToDoItem>(jsonFileStream, cancellationToken: ct);
             }
             return null;
         }
         async Task IToDoRepository.UpdateAsync(ToDoItem item, CancellationToken ct)
         {
-            await ((IToDoRepository)this).AddAsync(item, ct);
+            Directory.CreateDirectory(Path.Combine(_path, $"{item.User.UserId}"));
+            await File.WriteAllTextAsync(Path.Combine(_path, $"{item.User.UserId}", $"{item.Id}.json"), await ObjestToJsonStringAsync(item, ct), ct);
         }
     }
 }
