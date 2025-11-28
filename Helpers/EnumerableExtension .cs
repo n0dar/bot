db469ad811e2b193f2b7722da71658d55ebba180
n0dar <ru@ru.ru>
2025-11-29 01:33:11 +0300

    Урок 33. ДЗ (п. 2)

---
+++ Helpers/EnumerableExtension .cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace bot.Helpers
+{
+    public static class EnumerableExtension
+    {
+        public static IEnumerable<int> GetBatchByNumber(int batchSize, int batchNumber)
+        {
+            return Enumerable.Range(batchNumber * batchSize, batchSize);
+        }
+    }
+}
