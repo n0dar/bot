0ac7ad416488eaa0dd5f6f33d6fd641eb1ed89b9
n0dar <ru@ru.ru>
2025-12-09 01:50:09 +0300

    п. 1

---
+++ Core/Entities/Notification .cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace bot.Core.Entities
+{
+    internal class Notification
+    {
+        Guid Id { get; set; }
+        ToDoUser User { get; set; }
+        string Type { get; set; }//Тип нотификации. Например: DeadLine_{ToDoItem.Id}, Today_{DateOnly.FromDateTime(DateTime.UtcNow)}
+        string Text { get; set; }//Текст, который будет отправлен
+        DateTime ScheduledAt { get; set; }//Запланированная дата отправки
+        bool IsNotified { get; set; }//Флаг отправки
+        DateTime? NotifiedAt { get; set; }//Фактическая дата отправки
+    }
+}
